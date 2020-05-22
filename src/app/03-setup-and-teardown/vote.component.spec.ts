ed87714ff6aa25b49fca7f1a05f2752f56bd77dc
santyago.git <santyago.lp@gmail.com>
2020-05-21 22:08:37 -0300

    voteComponentTesting

---
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -0,0 +1,21 @@
+import { VoteComponent } from './vote.component';
+
+describe('VoteComponent', () => {
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
+  });
+
+  it('should increment totalVotes when upVoted', () => {
+    component.upVote();
+
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downvotes', () => {
+    component.downVote();
+
+    expect(component.totalVotes).toBe(-1);
+  });
+});
